62807f94341a638a774396a63872357d98272f47
Mahir <mahdimahir45@gmail.com>
2023-10-22 14:21:14 +0600

    created index file that exports all utils

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { axiosInstance } from "./axios";
+export { generateFilter } from "./generateFilter";
+export { generateSort } from "./generateSort";
+export { mapOperator } from "./mapOperator";
